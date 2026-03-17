0dbdf9bfc9ee4ab2d45ddb4e07e33042e3336b37
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-03-17 09:36:32 -0400

    feat: undo item with wip menu

--- BP/scripts/items/undo/events.js
+++ BP/scripts/items/undo/events.js
@@ -1,11 +1,29 @@
 import { TYPE_IDS } from "../../constants.js"
 import { Edit } from "../../edit/index.js"
 import { world } from "@minecraft/server"
+import { SelectionGroup } from "../../selection/selectionGroup.js"
+import { UndoMenu } from "./form.js"
 
-world.afterEvents.itemUse.subscribe((data) => {
+world.afterEvents.itemUse.subscribe(async (data) => {
     const { source, itemStack } = data
 
     if (itemStack.typeId !== TYPE_IDS.UNDO_ITEM) return
 
-    Edit.playerUndoRecent(source)
+    if (!source.customIsShifting) {
+        const { blocks } = await Edit.playerUndoRecent(source)
+
+        const group = SelectionGroup.get(source.id)
+
+        if (group) {
+            group.reloadArrowLocations()
+            group.reloadCoreLocation()
+            group.updateOriginalLocations()
+        }
+
+        if (blocks > 1000) {
+            player.sendMessage(blocks + " blocks filled")
+        }
+    } else {
+        new UndoMenu(source).mainMenu()
+    }
 })
