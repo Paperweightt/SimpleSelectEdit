c71edbada510cee5070801b32d3e7de7128f7a2d
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-03-18 07:13:52 -0400

    feat: getall method

--- BP/scripts/utils/player.js
+++ BP/scripts/utils/player.js
@@ -3,6 +3,50 @@
 import { system, world } from "@minecraft/server"
 import { PACK_ID } from "../constants"
 
+export class PlayerId {
+    /** @type {Record<string,string>} */
+    static idToName = {}
+    static propertyIdentifier = PACK_ID + ":player_id_to_name"
+
+    /**
+     * @param {string}
+     * @returns {string|undefined}
+     */
+    static getName(playerId) {
+        return this.idToName[playerId]
+    }
+
+    /**
+     * @return {{name:string,id:string}[]}
+     */
+    static getAll() {
+        return Object.entries(this.idToName).map(([id, name]) => {
+            return { id, name }
+        })
+    }
+
+    static save() {
+        world.setDynamicProperty(this.propertyIdentifier, JSON.stringify(this.idToName))
+    }
+
+    static innit() {
+        this.idToName = JSON.parse(
+            world.getDynamicProperty(this.propertyIdentifier) || "{}",
+        )
+    }
+}
+
+system.run(() => {
+    PlayerId.innit()
+})
+
+world.afterEvents.playerJoin.subscribe((data) => {
+    const { playerId, playerName } = data
+
+    PlayerId.idToName[playerId] = playerName
+    PlayerId.save()
+})
+
 export class PlayerUtils {
     /**
      * @param {import("@minecraft/server").Player}
@@ -20,39 +64,3 @@ export class PlayerUtils {
 
     static id = PlayerId
 }
-
-export class PlayerId {
-    /** @type {Record<string,string>} */
-    static idToName = {}
-
-    static propertyIdentifier = PACK_ID + ":player_id_to_name"
-
-    /**
-     * @param {string}
-     * @returns {string|undefined}
-     */
-    static getName(playerId) {
-        return this.idToName[playerId]
-    }
-
-    static save() {
-        world.setDynamicProperty(this.propertyIdentifier, JSON.stringify(this.idToName))
-    }
-
-    static innit() {
-        this.idToName = JSON.parse(
-            world.getDynamicProperty(this.propertyIdentifier) || "{}",
-        )
-    }
-}
-
-world.afterEvents.playerJoin.subscribe((data) => {
-    const { playerId, playerName } = data
-
-    Id.idToName[playerId] = playerName
-    Id.save()
-})
-
-system.run(() => {
-    Id.innit()
-})
